d765ed0da45e70cfac5517c42a7dee4e4035f626
Diego Martínez <a353198@uach.mx>
2023-11-24 10:55:48 -0600

    + rolesController +roleModel

--- controllers/roles.js
+++ controllers/roles.js
@@ -1,27 +1,90 @@
 const express = require('express');
+const Role = require('../models/role')
 
 function create(req, res, next){
-    res.send('Role create')
+    let role = req.body.role;
+
+    let userRole = new Role({
+        role: role
+    });
+
+    userRole.save().then(obj => res.status(200).json({
+        msg: res.__('roles.create.ok'),
+        obj: obj
+    })).catch(ex => res.status(500).json({
+        msg: res.__('roles.create.wrong'),
+        obj: ex
+    }));
 }
 
 function list(req, res, next) {
-    res.send('Roles list');
+    let page = req.params.page? req.params.page :1;
+    const options = {
+        page: page,
+        limit: 5
+    };
+    Role.paginate({},options).then(objs => res.status(200).json({
+        msg: res.__('roles.list.ok'),
+        obj: objs
+    })).catch(ex => res.status(500).json({
+        msg: res.__('roles.list.wrong'),
+        obj: ex
+    }));
 }
 
 function index(req, res, next){
-    res.send('Role index')
+    const id = req.params.id;
+    User.findOne({"_id":id}).then(obj => res.status(200).json({
+        msg: res.__('roles.index.ok')+`${id}`,
+        obj: obj
+    })).catch(ex => res.status(500).json({
+        msg: res.__('roles.index.wrong')+`${id}`,
+        obj: ex
+    }));
 }
 
 function replace(req, res, next){
-    res.send('Role replace')
+    const id = req.params.id;
+    const role = req.body.role ? req.body.role : "";
+
+    const userRole = new Object({
+        _role: role,
+    });
+
+    Role.findOneAndUpdate({"_id":id}, userRole, {new: true}).then(obj => res.status(200).json({
+        msg: res.__('roles.replace.ok')+`${id}`,
+        obj: obj
+    })).catch(ex => res.status(500).json({
+        msg: res.__('roles.replace.wrong')+`${id}`,
+        obj: ex
+    }));
 }
 
 function update(req, res, next){
-    res.send('Role update')
+    const id = req.params.id;
+    const role = req.body.role;
+
+    const userRole = new Object();
+    if(role) userRole._role = role;
+
+    Role.findOneAndUpdate({"_id":id}, userRole).then(obj => res.status(200).json({
+        msg: res.__('roles.update.ok'),
+        obj: obj
+    })).catch(ex => res.status(500).json({
+        msg: res.__('roles.update.wrong')+`${id}`,
+        obj: ex
+    }));
 }
 
 function destroy(req, res, next){
-    res.send('Role destroy')
+    const id = req.params.id;
+    Role.findByIdAndDelete({"_id":id}).then(obj => res.status(200).json({
+        msg: res.__('roles.destroy.ok')+`${id}`,
+        obj: obj
+    })).catch(ex => res.status(500).json({
+        msg:  res.__('roles.destroy.wrong')+`${id}`,
+        obj: ex
+    }));
 }
 
 module.exports = {
--- models/role.js
+++ models/role.js
@@ -1,4 +1,21 @@
+const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
+const schema = mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true},
+    _role: String
+});
 
+class Role {
+    constructor(role){
+        this._role = role;
+    }
+
+    get role(){ return this._role; }
+    set role(v){ this._role = v; }
+}
+
+schema.loadClass(Role);
 schema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Role', schema);
