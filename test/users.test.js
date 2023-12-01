d964f089faae8e957673c04c90846e4004c6eff2
Daniel Lozano <a348603@uach.mx>
2023-11-30 18:18:09 -0600

    Creando esqueletos de test para storyCards, teams y users

--- test/users.test.js
+++ test/users.test.js
@@ -1,1 +1,172 @@
 //d
+const supertest = require('supertest');
+
+const app = require('../app');
+
+describe("Probar create de user", () => {
+    it("Deberia de crear un user", (done) => {
+        supertest(app).post('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+    
+    it("Deberia de no crear un user", (done) => {
+        supertest(app).post('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+})
+
+describe("Probar get de user", () => {
+    it("Deberia de obtener un user", (done) => {
+        supertest(app).get('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+    
+    it("Deberia de no obtener un user", (done) => {
+        supertest(app).get('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+})
+
+describe("Probar get list de user", () => {
+    it("Deberia de obtener una lista de users", (done) => {
+        supertest(app).get('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+    
+    it("Deberia de no obtener una lista de users", (done) => {
+        supertest(app).get('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+})
+
+describe("Probar put user", () => {
+    it("Deberia de modificar un atributo de un user", (done) => {
+        supertest(app).put('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+    
+    it("Deberia de no modificar un atributo de un user", (done) => {
+        supertest(app).put('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+})
+
+describe("Probar patch user", () => {
+    it("Deberia de actualizar un user", (done) => {
+        supertest(app).patch('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+    
+    it("Deberia de no actualizar un user", (done) => {
+        supertest(app).patch('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+})
+
+describe("Probar delete user", () => {
+    it("Deberia de eliminar un user", (done) => {
+        supertest(app).delete('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+    
+    it("Deberia de no eliminar un user", (done) => {
+        supertest(app).delete('/users')
+        .send({})
+        .expect(200)
+        .end(function(err, res){
+            if(err){
+                done(err);
+            } else {
+                done();
+            };
+        })
+    })
+})
